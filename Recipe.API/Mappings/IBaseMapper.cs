c0a166d4a097109ca13b8b45daa7dff93842a695
barisvarlik <barisvarlik19@gmail.com>
2023-09-22 13:42:04 +0300

    GenericService class and IBaseMapper interface implemented.

---
+++ Recipe.API/Mappings/IBaseMapper.cs
@@ -0,0 +1,12 @@
+﻿namespace Recipe.API.Mappings;
+
+public interface IBaseMapper<TEntity, TReadDto, TCreateDto, TUpdateDto> 
+    where TEntity : class 
+    where TReadDto : class 
+    where TCreateDto : class 
+    where TUpdateDto : class
+{
+    TEntity ToEntity(TCreateDto dto);
+    TEntity ToEntity(TUpdateDto dto, TEntity entity);
+    TReadDto ToDto(TEntity entity);
+}
---
+++ Recipe.API/Services/GenericService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recipe.API.Mappings;
+using Recipe.Repository;
+
+namespace Recipe.API.Services;
+
+public class GenericService<TEntity, TReadDto, TCreateDto, TUpdateDto> 
+    where TEntity : class 
+    where TReadDto : class 
+    where TCreateDto : class 
+    where TUpdateDto : class
+{
+    private readonly AppDbContext _context;
+    private readonly IBaseMapper<TEntity, TReadDto, TCreateDto, TUpdateDto> _mapper;
+
+    public GenericService(AppDbContext context, IBaseMapper<TEntity, TReadDto, TCreateDto, TUpdateDto> mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    
+    public virtual async Task<IEnumerable<TReadDto>> GetAllAsync()
+    {
+        var entities = await _context.Set<TEntity>().AsNoTracking().ToListAsync();
+        return entities.Select(entity => _mapper.ToDto(entity));
+    }
+    
+    public virtual async Task<TReadDto> GetByIdAsync(int id)
+    {
+        var entity = await _context.Set<TEntity>().FindAsync(id);
+        if (entity == null) throw new KeyNotFoundException($"Entity not found with id {id}");
+        return _mapper.ToDto(entity);
+    }
+    
+    public virtual async Task<TReadDto> CreateAsync(TCreateDto dto)
+    {
+        var entity = _mapper.ToEntity(dto);
+        await _context.Set<TEntity>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return _mapper.ToDto(entity);
+    }
+    
+    public virtual async Task<TReadDto> UpdateAsync(int id, TUpdateDto dto)
+    {
+        var entity = await _context.Set<TEntity>().FindAsync(id);
+        if (entity == null) throw new KeyNotFoundException($"Entity not found with id {id}");
+        
+        _mapper.ToEntity(dto, entity);
+        _context.Set<TEntity>().Update(entity);
+        await _context.SaveChangesAsync();
+        
+        return _mapper.ToDto(entity);
+    }
+    
+    public virtual async Task DeleteAsync(int id)
+    {
+        var entity = await _context.Set<TEntity>().FindAsync(id);
+        if (entity == null) throw new KeyNotFoundException($"Entity not found with id {id}");
+        
+        _context.Set<TEntity>().Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+}
